0dcab6aea93aeb6b6da4b4abf66a0bdf146247e2
Student <example@example.com>
2019-07-31 17:42:38 -0700

    empty out array

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import AddUserForm from './forms/AddUserForm'
 import EditUserForm from './forms/EditUserForm'
 
 const App = () => {
-  const usersData = [
-    { id: 1, name: 'Tania', username: 'floppydiskette' },
-    { id: 2, name: 'Craig', username: 'siliconeidolon' },
-    { id: 3, name: 'Ben', username: 'benisphere' },
-  ]
+  const usersData = []
 
   const [users, setUsers] = useState(usersData)
   const [editing, setEditing] = useState(false)
